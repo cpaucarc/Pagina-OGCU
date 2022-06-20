42c6943cf092ec23abfc623e8242fcdcaebb3f3b
Frank Paucar <fcpaucar@gmail.com>
2022-06-20 17:41:05 -0500

    :art: agrega el enlace al SGC en la seccion portal

--- php/correo.php
+++ php/correo.php
@@ -1,22 +1,19 @@
 <?php
 
-if(isset($_POST['enviar'])){
-    if(!empty($_POST['nombre']) && !empty($_POST['email']) && !empty($_POST['asunto']) && !empty($_POST['mensaje'])){
+if (isset($_POST['enviar'])) {
+    if (!empty($_POST['nombre']) && !empty($_POST['email']) && !empty($_POST['asunto']) && !empty($_POST['mensaje'])) {
 
-        $destino = "ogcu-secretaria@unasam.edu.pe";
+        $destino = "correofalso@yopmail.com";
         $nombre = $_POST['nombre'];
         $email = $_POST['email'];
         $asunto = $_POST['asunto'];
         $mensaje = $_POST['mensaje'];
-        
+
         $contenido = "Nombre: " . $nombre . "\nCorreo: " . $email . "\nMensaje: " . $mensaje;
         $mail =  mail($destino, $asunto, $contenido);
-        if($mail){
-            
-            echo "<h4> enviado exitosamente </h4>";
+        if ($mail) {
 
+            echo "<h4> Mensaje enviado exitosamente.</h4>";
         }
     }
 }
-
-?>  
--- php/correo_suge.php
+++ php/correo_suge.php
@@ -1,23 +1,20 @@
 <?php
 
-if(isset($_POST['enviar'])){
-    if(!empty($_POST['nombre']) && !empty($_POST['email']) && !empty($_POST['asunto']) && !empty($_POST['mensaje'])){
+if (isset($_POST['enviar'])) {
 
-        $destino = "ogcu-secretaria@unasam.edu.pe";
+    if (!empty($_POST['nombre']) && !empty($_POST['email']) && !empty($_POST['asunto']) && !empty($_POST['sugerencia'])) {
+
+	$destino = "correofalso@yopmail.com";
         $nombre = $_POST['nombre'];
         $email = $_POST['email'];
         $asunto = $_POST['asunto'];
-        $mensaje = $_POST['mensaje'];
-        
+        $mensaje = $_POST['sugerencia'];
+
         $contenido = "Nombre: " . $nombre . "\nCorreo: " . $email . "\nMensaje: " . $mensaje;
         $mail =  mail($destino, $asunto, $contenido);
-        if($mail){
-            
-            echo "<h4> enviado exitosamente </h4>";
 
+        if ($mail) {
+            echo "<h4>Mensaje enviado exitosamente.</h4>";
         }
     }
 }
-
-?>  
-
